42db9abe3373fbb04a9ec30c3e2d550b774e4606
One <appdevoney@gmail.com>
2014-12-01 20:39:32 +0800

    Init

---
+++ config/settings.inc.php
@@ -0,0 +1,19 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'prestashop');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', 'root');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_MEDIA_SERVER_1_', '');
+define('_MEDIA_SERVER_2_', '');
+define('_MEDIA_SERVER_3_', '');
+define('_COOKIE_KEY_', 'czL1vVpQ05aOeo5YUFrd0MGBRqryefdoY84Bgq5bQjrGa0oU5Q9ieGXN');
+define('_COOKIE_IV_', 'UsqWovzi');
+define('_PS_CREATION_DATE_', '2014-12-01');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.0.9');
+define('_RIJNDAEL_KEY_', '2aGb42RBrZ7U0LCLYhyAKNsvZp799gz3');
+define('_RIJNDAEL_IV_', 'Wgm8iUOugw3cMSUve2ds0g==');
